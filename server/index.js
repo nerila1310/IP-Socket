d7da867050a3a6619e62c552a1e513793c44d8d7
Neri-Laredo <nerila1310@gmail.com>
2020-05-07 20:08:58 -0500

    Emitiendo un mensaje al cliente desde server

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+app.get('/hola-mundo', function(req, res){
+	res.status(200).send('hola mundo desde una ruta');
+})
+
 /*===========================================
 =            Creamos el servidor            =
 ===========================================*/
