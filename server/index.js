8b2aa2bb3f17cbb421550c2709dc28e5ff537f9b
Neri-Laredo <nerila1310@gmail.com>
2020-05-07 20:47:00 -0500

    Enviando mensaje de Bienvenida

--- server/index.js
+++ server/index.js
@@ -9,10 +9,15 @@ app.get('/hola-mundo', function(req, res){
 	res.status(200).send('hola mundo desde una ruta');
 })
 
+var messages = [{
+	id: 1,
+	text: 'Bienvenido al chat privado de Socket.io y NodeJS de Neri Laredo...',
+	nickname: 'Bot - NeriLaredo'
+}];
+
 io.on('connection', function(socket){
-
 	console.log('El nodo con IP: '+ socket.handshake.address +' se ha conectado...');
-
+	socket.emit('messages', messages);
 });
 
 /*===========================================
