de8394961cc3316e141df3a631292502aa18188a
Neri-Laredo <nerila1310@gmail.com>
2020-05-07 21:35:16 -0500

    Enviando y reciviendo mensajes

--- client/main.js
+++ client/main.js
@@ -12,9 +12,23 @@ function render(data){
 						<strong> ${message.nickname} </strong> dice:
 						<p> ${message.text} </p>	
 					</div>
-	
 			`);
 	}).join(' ');
+	var div_msgs = document.getElementById('messages');
+	div_msgs.innerHTML = html;
+	div_msgs.scrollTop = div_msgs.scrollHeight;
+}
 
-	document.getElementById('messages').innerHTML = html;
+function addMessage(e){
+	var message = {
+		nickname: document.getElementById("nickname").value,
+		text: document.getElementById('text').value
+	};
+
+	document.getElementById('nickname').style.display = 'none';
+	socket.emit('add-message', message);
+
+	document.getElementById('text').value = '';
+
+	return false;
 }
--- server/index.js
+++ server/index.js
@@ -18,6 +18,12 @@ var messages = [{
 io.on('connection', function(socket){
 	console.log('El nodo con IP: '+ socket.handshake.address +' se ha conectado...');
 	socket.emit('messages', messages);
+
+	socket.on('add-message', function(data){
+		messages.push(data);
+		
+		io.sockets.emit('messages', messages);
+	});
 });
 
 /*===========================================
